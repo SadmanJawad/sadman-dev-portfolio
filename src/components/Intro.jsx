eb0465057ac52ebbc1b3cdf9416615a99790a021
SadmanJawad <jawadnabil977@gmail.com>
2023-07-02 01:18:55 +0600

    intro updated

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,8 +4,15 @@ const Intro = () => {
     return (
         <div className='flex items-center justify-center flex-col text-center pt-20 pb-6 '>
           <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Sadman Jawad</h1>
-          <p>Full Stack Developer</p>
-          <p>This is my entire bio</p>
+          <p className='text-base md:text-xl mb-3 font-medium'>Full Stack Developer</p>
+          <p className='text-sm max-w-xl mb-6 font-bold'>I'm a passionate full-stack developer <br /> with a knack for crafting exceptional digital experiences.
+           With a strong background in both front-end and back-end development,
+           <br /> I have successfully delivered numerous projects, 
+          
+           showcasing their expertise in creating visually stunning user interfaces and seamless interactions.
+           Their attention to detail and design sense sets them apart, 
+         
+           creating immersive online experiences for users.</p>
         </div>
     );
 };
