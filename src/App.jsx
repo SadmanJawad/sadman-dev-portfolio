c1273aca9264318ec4fd7ada94f8ba1256efcd09
SadmanJawad <jawadnabil977@gmail.com>
2023-07-02 00:53:23 +0600

    setting boiler platet

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import Intro from "./components/Intro"
+import Portfolio from "./components/Portfolio"
+import Timeline from "./components/Timeline"
+
+
+function App() {
+
+  return (
+    <div>
+      <Intro></Intro>
+      <Portfolio></Portfolio>
+      <Timeline></Timeline>
+      <Contact></Contact>
+      <Footer></Footer>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Intro = () => {
+    return (
+        <div className='flex items-center justify-center flex-col text-center pt-20 pb-6 '>
+          <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Sadman Jawad</h1>
+          <p>Full Stack Developer</p>
+          <p>This is my entire bio</p>
+        </div>
+    );
+};
+
+export default Intro;
