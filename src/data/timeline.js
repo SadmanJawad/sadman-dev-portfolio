89b4162680de64e5a0fa975deadb864f06913968
SadmanJawad <jawadnabil977@gmail.com>
2023-07-02 16:44:23 +0600

    timeline,contact and footer added

---
+++ src/data/timeline.js
@@ -0,0 +1,23 @@
+export default [
+    {
+       year: '2023',
+       title: 'MERN Stack Developer',
+       duration: '8 months',
+       details: 
+          'My humble attempt at building my version of the digital world one step at a time by documenting my coding journey and creating software engineering and computer science content to help those coming behind me.',
+    },
+    {
+         year: '2018-2022',
+         title: 'BSc in Electrical & Electronics Engineering',
+         duration: '4 years',
+         details:
+             "looking forward to learn more about the field of electrical engineering and electronics and to be able to apply what I've learned in the real world.",
+     },
+     {
+         year: '2014-2016',
+         title: 'Higher Secondary School Certificate',
+         duration: '2 years',
+         details:
+             'I studied science in Government Science College, Dhaka. I was a student of the science group. I studied physics, chemistry, biology, and mathematics.',
+     }
+ ]
