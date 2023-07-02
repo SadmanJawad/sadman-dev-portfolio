b9610bf318785616aba19e4d754cc108af367085
SadmanJawad <jawadnabil977@gmail.com>
2023-07-02 13:48:23 +0600

    portfolio component added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import PortfolioItem from './PortfolioItem';
+import portfolio from '../data/portfolio'
 
 const Portfolio = () => {
     return (
-        <div>
-            <h1>Portfolio</h1>
+        <div className='flex flex-col md:flex-row items-center justify-center'>
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+            {
+                portfolio.map(project => (
+                    <PortfolioItem key={project.id}
+                    imgURL={project.imgURL}
+                    title={project.title}
+                    stack={project.stack}
+                    link={project.link}
+                    >  </PortfolioItem>
+                ))
+            }
+           </div>
         </div>
     );
 };
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {   id: 1,
+        title: 'Project 1',
+        imgURL: '',
+        stack: ['React', 'Node', 'Express', 'MongoDB'],
+        link: 'https://abc.com'
+    },
+    {   id: 2,
+        title: 'Project 2',
+        imgURL: '',
+        stack: ['React', 'Node', 'Express', 'MongoDB'],
+        link: 'https://abc.com'
+    },
+    {   id: 3,
+        title: 'Project 1',
+        imgURL: '',
+        stack: ['React', 'Node', 'Express', 'MongoDB'],
+        link: 'https://abc.com'
+    },
+]
